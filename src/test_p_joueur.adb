543abdeedc16cace420fd8c8077ce665721ee413
Hugog4223 <65758922+Hugog4223@users.noreply.github.com>
2020-07-02 11:12:50 +0200

    bloqué
    bloqué

--- src/test_p_joueur.adb
+++ src/test_p_joueur.adb
@@ -1,5 +1,5 @@
-with P_Carte, Ada.Text_IO, P_Aleatoire, P_Utils, P_joueur, Ada.Integer_Text_IO;
-use P_Carte, Ada.Text_IO, P_Utils, P_carte, P_joueur, Ada.Integer_Text_IO;
+with P_Carte, Ada.Text_IO, P_Aleatoire, P_Utils, P_joueur, Ada.Integer_Text_IO, P_Action;
+use P_Carte, Ada.Text_IO, P_Utils, P_carte, P_joueur, Ada.Integer_Text_IO, P_Action;
 
 
 procedure Test_P_joueur is
@@ -8,42 +8,65 @@ procedure Test_P_joueur is
    use P_Carte_Alea;
    
    deck : T_Deck := deckComplet;
-   joueur : T_Joueur;
+   joueur1 : T_Joueur;
+   joueur2 : T_Joueur;
    nom : string := "jammich";
+   nomm : string := "jeanne";
+   action : T_action(miser);
 begin
+   
+   Put_Line("-----");
+   Put_Line("Test du package P_joueur");
+   Put_Line("-----");
+   New_Line;
    resetGenerator; -- permet d'obtenir des cartes différentes lors des tirages
    deck := shuffle(deck'First,deck'Last,deck);
-   joueur := creerJoueur(nom, 5000);
-   prendreCartes(deck, joueur);
-   gagnerMise(50, joueur);
-   Put_Line("premier test");
-   put(toString(joueur));
-   put("----------");
-   put(getArgent(joueur));
-   put("----------");
-   put(getMise(joueur));
-   put("----------");
-   put(getName(joueur));
-   put("----------");
-   put(toString(getCartes(joueur)));
-   put("----------");
-   put(boolean'image(isPlaying(joueur)));
+   joueur1 := creerJoueur(nom, 5000);
+   prendreCartes(deck, joueur1);
+   gagnerMise(50, joueur1);
+   Put_Line("affichage des données du joueur contenue dans le type T_joueur");
+   put(toString(joueur1));
    put("----------");
    New_Line;
-   Put_Line("deuxième test");
-   jouerTour(0, joueur , action : miser);
-   put(toString(joueur));
+   Put_Line("affichage des données du joueur une par une");
+   put(getArgent(joueur1));
    put("----------");
-   put(getArgent(joueur));
+   put(getMise(joueur1));
    put("----------");
-   put(getMise(joueur));
+   put(getName(joueur1));
    put("----------");
-   put(getName(joueur));
+   put(toString(getCartes(joueur1)));
    put("----------");
-   put(toString(getCartes(joueur)));
+   put(boolean'image(isPlaying(joueur1)));
    put("----------");
-   put(boolean'image(isPlaying(joueur)));
-   put("----------");
+   New_Line;
    
+   Put_Line("On défini un deuxième joueur");
+   deck := shuffle(deck'First,deck'Last,deck);
+   joueur2 := creerJoueur(nomm, 5000);
+   prendreCartes(deck, joueur2);
+   Put_Line("affichage des données du joueur");
+   put(toString(joueur2));
+   put("----------");
+   New_Line;
    
+   Put_Line("*********");
+   if jouerTour(0, joueur2 , action)= True then
+      put(toString(joueur2));
+      put("----------");
+   else
+      put("tour non joue");
+   end if;
+   New_line;
+   
+   poserBlinde(joueur2, 50);
+   if jouerTour(50, joueur1 , action)= True then
+      put(toString(joueur1));
+      put("----------");
+   else 
+      put("tour non joue"); 
+   end if;
+   New_line;
+   --put(toString(joueur2));
+   put("----------");
 end Test_P_joueur;
